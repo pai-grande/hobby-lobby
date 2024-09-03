4ff30586b6d60b963fc7e84433913e31d759b239
javaiboltz <jonny.l.nascimento@gmail.com>
2024-09-03 11:57:56 +0100

    Commented stuff to allow deployment

--- src/app/ui/dashboard/sidenav.tsx
+++ src/app/ui/dashboard/sidenav.tsx
@@ -10,7 +10,7 @@ export default function SideNav() {
         className="mb-2 flex h-20 items-end justify-start rounded-md bg-blue-600 p-4 md:h-40"
         href="/"
       >
-        <div className="w-32 text-white md:w-40">Root</div>
+        <div className="w-32 text-white md:w-40">{"Root"}</div>
       </Link>
       <div className="flex grow flex-row justify-between space-x-2 md:flex-col md:space-x-0 md:space-y-2">
         <NavLinks />
@@ -18,7 +18,7 @@ export default function SideNav() {
         <form>
           <button className="flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3">
             <PowerIcon className="w-6" />
-            <div className="hidden md:block">Sign Out</div>
+            <div className="hidden md:block">{"Sign Out"}</div>
           </button>
         </form>
       </div>
