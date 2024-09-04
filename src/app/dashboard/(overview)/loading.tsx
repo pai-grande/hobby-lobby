ec0f14ba5ed8bffaf826e945d275ec486ea48b42
javaiboltz <jonny.l.nascimento@gmail.com>
2024-09-04 12:30:57 +0100

    Commented import/action/form from shadcn. Added Suspense and Skeletons to dashboard loading.

---
+++ src/app/dashboard/(overview)/loading.tsx
@@ -0,0 +1,6 @@
+import DashboardSkeleton from "@/components/ui/skeletons";
+
+export default function Loading() {
+  //   return <div>Loading...</div>;
+  return <DashboardSkeleton />;
+}
---
+++ src/app/dashboard/(overview)/page.tsx
@@ -0,0 +1,84 @@
+import LatestInvoices from "@/components/ui/dashboard/latest-invoices";
+import RevenueChart from "@/components/ui/dashboard/revenue-chart";
+import { lusitana } from "@/components/ui/dashboard/fonts";
+import CardWrapper, { Card } from "@/components/ui/dashboard/cards";
+import { fetchCardData } from "../../lib/data";
+import { Suspense } from "react";
+import {
+  LatestInvoicesSkeleton,
+  RevenueChartSkeleton,
+} from "@/components/ui/skeletons";
+
+// Card wrapper : multiple components load in at the same time
+
+export default async function Page() {
+  return (
+    <main>
+      <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
+        Dashboard
+      </h1>
+      <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
+        <Suspense fallback={<CardWrapper />}>
+          <CardWrapper />
+        </Suspense>
+      </div>
+      <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-3 lg:grid-cols-8">
+        <Suspense fallback={<RevenueChartSkeleton />}>
+          <RevenueChart />
+        </Suspense>
+        <Suspense fallback={<LatestInvoicesSkeleton />}>
+          <LatestInvoices />
+        </Suspense>
+      </div>
+    </main>
+  );
+  //<p>Dashboard Page</p>;)
+}
+
+// const formSchema = z.object({
+//   username: z.string().min(2).max(50),
+// });
+// const form = useForm<z.infer<typeof formSchema>>({
+//   resolver: zodResolver(formSchema),
+//   defaultValues: {
+//     username: "",
+//   },
+// });
+// function onSubmit(values: z.infer<typeof formSchema>) {
+//   console.log(values);
+// }
+
+// const action = useAction(insertUsername, {
+//   onSuccess: () => {
+//     toast.success("Username inserted successfully");
+//   },
+//   onError: () => {
+//     toast.error("Server error");
+//   },
+// });
+
+{
+  /* <div>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+            <FormField
+              control={form.control}
+              name="username"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Username</FormLabel>
+                  <FormControl>
+                    <Input placeholder="your username..." {...field} />
+                  </FormControl>
+                  <FormDescription>
+                    This is your public display name.
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button type="submit">Submit</Button>
+          </form>
+        </Form>
+      </div> */
+}
