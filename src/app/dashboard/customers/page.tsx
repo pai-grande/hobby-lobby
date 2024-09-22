836c2265ee4b29ef5b5117f6909276059f7548fe
pai-grande <jonny.l.nascimento@gmail.com>
2024-09-22 16:40:44 +0100

    Work on wardrobe form and customer page

--- src/app/dashboard/customers/page.tsx
+++ src/app/dashboard/customers/page.tsx
@@ -1,5 +1,8 @@
-import { getRow } from "@/app/lib/actions_supabase";
+import WardrobeForm from "@/components/shared/wardrobeForm";
 import { Button } from "@/components/ui/shadcn/button";
+import { Input } from "@/components/ui/shadcn/input";
+import { Label } from "@/components/ui/shadcn/label";
+import { getRow } from "@/app/lib/actions_supabase";
 import {
   Card,
   CardHeader,
@@ -8,8 +11,7 @@ import {
   CardContent,
   CardFooter,
 } from "@/components/ui/shadcn/card";
-import { Input } from "@/components/ui/shadcn/input";
-import { Label } from "@/components/ui/shadcn/label";
+
 import {
   Select,
   SelectContent,
@@ -25,44 +27,47 @@ export default async function Page() {
   // display result
 
   return (
-    <Card className="w-[350px]">
-      <CardHeader>
-        <CardTitle>Upload clothing</CardTitle>
-        <CardDescription>
-          Add a piece of clothing to your wardrobe
-        </CardDescription>
-      </CardHeader>
-      <CardContent>
-        <form>
-          <div className="grid w-full items-center gap-4">
-            <div className="flex flex-col space-y-1.5">
-              <Label htmlFor="name">Name</Label>
-              <Input id="name" placeholder="Name of your piece clothing" />
+    <>
+      <WardrobeForm />
+      <Card className="w-[350px]">
+        <CardHeader>
+          <CardTitle>Upload clothing</CardTitle>
+          <CardDescription>
+            Add a piece of clothing to your wardrobe
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form>
+            <div className="grid w-full items-center gap-4">
+              <div className="flex flex-col space-y-1.5">
+                <Label htmlFor="name">Name</Label>
+                <Input id="name" placeholder="Name of your piece clothing" />
+              </div>
+              <div className="flex flex-col space-y-1.5">
+                <Label htmlFor="framework">Type</Label>
+                <Select>
+                  <SelectTrigger id="framework">
+                    <SelectValue placeholder="Select" />
+                  </SelectTrigger>
+                  <SelectContent position="popper">
+                    <SelectItem value="next">Head</SelectItem>
+                    <SelectItem value="nuxt">Coat</SelectItem>
+                    <SelectItem value="astro">Sweater</SelectItem>
+                    <SelectItem value="sveltekit">Shirt</SelectItem>
+                    <SelectItem value="sveltekit">Trousers</SelectItem>
+                    <SelectItem value="sveltekit">Socks</SelectItem>
+                    <SelectItem value="sveltekit">Shoes</SelectItem>
+                  </SelectContent>
+                </Select>
+              </div>
             </div>
-            <div className="flex flex-col space-y-1.5">
-              <Label htmlFor="framework">Type</Label>
-              <Select>
-                <SelectTrigger id="framework">
-                  <SelectValue placeholder="Select" />
-                </SelectTrigger>
-                <SelectContent position="popper">
-                  <SelectItem value="next">Head</SelectItem>
-                  <SelectItem value="nuxt">Coat</SelectItem>
-                  <SelectItem value="astro">Sweater</SelectItem>
-                  <SelectItem value="sveltekit">Shirt</SelectItem>
-                  <SelectItem value="sveltekit">Trousers</SelectItem>
-                  <SelectItem value="sveltekit">Socks</SelectItem>
-                  <SelectItem value="sveltekit">Shoes</SelectItem>
-                </SelectContent>
-              </Select>
-            </div>
-          </div>
-        </form>
-      </CardContent>
-      <CardFooter className="flex justify-between">
-        <Button variant="outline">Cancel</Button>
-        <Button>Upload</Button>
-      </CardFooter>
-    </Card>
+          </form>
+        </CardContent>
+        <CardFooter className="flex justify-between">
+          <Button variant="outline">Cancel</Button>
+          <Button>Upload</Button>
+        </CardFooter>
+      </Card>
+    </>
   );
 }
---
+++ src/components/shared/WardrobeForm.tsx
@@ -0,0 +1,122 @@
+"use client";
+
+import { wardrobeFormSchema } from "@/app/lib/validator";
+import { uploadImage } from "@/app/lib/actions_supabase";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { FileUploader } from "../ui/FileUploader";
+import { useForm } from "react-hook-form";
+import { Textarea } from "../ui/textarea";
+import { Input } from "../ui/input";
+import { useState } from "react";
+import * as z from "zod";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage,
+} from "../ui/form";
+import { Button } from "../ui/button";
+
+const WardrobeForm = () => {
+  const [file, setFile] = useState<File | null>(null);
+  const initialValues = {
+    title: "",
+    description: "",
+    image: "",
+    category: "Head" as
+      | "Head"
+      | "Coat"
+      | "Sweater"
+      | "Shirt"
+      | "Trousers"
+      | "Socks"
+      | "Shoes",
+  };
+
+  const form = useForm<z.infer<typeof wardrobeFormSchema>>({
+    resolver: zodResolver(wardrobeFormSchema),
+    defaultValues: initialValues,
+  });
+  async function onSubmit(values: z.infer<typeof wardrobeFormSchema>) {
+    let uploadedImageUrl = values.image;
+    try {
+      if (file) {
+        const formData = new FormData();
+        formData.append("file", file);
+        uploadedImageUrl = (await uploadImage(formData)) || uploadedImageUrl;
+      }
+    } catch (error) {
+      console.log("Error while uploading file: ", error);
+    }
+  }
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="flex flex-col gap-5"
+      >
+        <div className="flex flex-col gap-5 md:flex-row">
+          <FormField
+            control={form.control}
+            name="title"
+            render={({ field }) => (
+              <FormItem className="w-full">
+                <FormControl>
+                  <Input
+                    placeholder="Clothing title"
+                    {...field}
+                    className="input-field"
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="description"
+            render={({ field }) => (
+              <FormItem className="w-full">
+                <FormControl>
+                  <Textarea
+                    placeholder="Description"
+                    {...field}
+                    className="textarea rounded-md"
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="image"
+            render={({ field }) => (
+              <FormItem className="w-full">
+                <FormControl>
+                  <FileUploader
+                    onFieldChange={field.onChange}
+                    imageUrl={field.value}
+                    setFile={(file: File | null) => setFile(file)}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </div>
+        <Button
+          type="submit"
+          size="lg"
+          disabled={form.formState.isSubmitting}
+          className="button col-span-2 w-full"
+        >
+          {form.formState.isSubmitting ? "Submitting..." : "Upload"}
+        </Button>
+      </form>
+    </Form>
+  );
+};
+
+export default WardrobeForm;
